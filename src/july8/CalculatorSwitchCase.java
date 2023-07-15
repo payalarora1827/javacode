db5818f029950a53a3d480d8ef37524b07ec0413
payalarora1827 <payalarora1827@gmail.com>
2023-07-14 23:33:56 -0700

    changes in calculator using loop

--- src/july8/CalculatorSwitchCase.java
+++ src/july8/CalculatorSwitchCase.java
@@ -2,64 +2,68 @@ package july8;
 import java.util.Scanner;
 public class CalculatorSwitchCase {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Press number");
-        System.out.println("Press 1");
-        System.out.println("Press 2");
-        System.out.println("Press 3");
-        System.out.println("Press 4");
-        System.out.println("Press 5");
-        int input = scanner.nextInt();
         int n1;
         int n2;
         int n3;
-
-        switch(input)
-        {
-            case 1 :
-                System.out.println("Enter your first number");
-                n1 = scanner.nextInt();
-                System.out.println("Enter your Second  number");
-                n2 = scanner.nextInt();
-                n3 = n1 + n2;
-                System.out.print(n3);
-                break;
-              case 2 :
-                    System.out.println("Enter your first number");
-                    n1 = scanner.nextInt();
-                    System.out.println("Enter your Second  number");
-                    n2 = scanner.nextInt();
-                    n3 = n1 - n2;
-                    System.out.print(n3);
-                    break;
-            case 3 :
-                System.out.println("Enter your first number");
-                n1  = scanner.nextInt();
-                System.out.println("Enter your Second  number");
-                n2 = scanner.nextInt();
-                n3  = n1 * n2;
-                System.out.print(n3);
-                break;
-            case 4 :
-                System.out.println("Enter your first number");
-                n1 = scanner.nextInt();
-                System.out.println("Enter your Second  number");
-                n2 = scanner.nextInt();
-                n3 = n1 % n2;
-                System.out.println(n3);
-                break;
-            case 5 :
-                System.out.println("Enter your first number");
-                n1 = scanner.nextInt();
-                System.out.println("Enter your Second  number");
-                n2 = scanner.nextInt();
-                n3 = n1 / n2;
-                System.out.println(n3);
-                break;
-            default :
-                System.err.println("Wrong Input");
-                break;
-        }
+        char ch;
+do {
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Press number");
+    System.out.println("Press 1");
+    System.out.println("Press 2");
+    System.out.println("Press 3");
+    System.out.println("Press 4");
+    System.out.println("Press 5");
+    int input = scanner.nextInt();
+    switch (input)
+    {
+        case 1:
+            System.out.println("Enter your first number");
+            n1 = scanner.nextInt();
+            System.out.println("Enter your Second  number");
+            n2 = scanner.nextInt();
+            n3 = n1 + n2;
+            System.out.println(n3);
+            break;
+        case 2:
+            System.out.println("Enter your first number");
+            n1 = scanner.nextInt();
+            System.out.println("Enter your Second  number");
+            n2 = scanner.nextInt();
+            n3 = n1 - n2;
+            System.out.println(n3);
+            break;
+        case 3:
+            System.out.println("Enter your first number");
+            n1 = scanner.nextInt();
+            System.out.println("Enter your Second  number");
+            n2 = scanner.nextInt();
+            n3 = n1 * n2;
+            System.out.println(n3);
+            break;
+        case 4:
+            System.out.println("Enter your first number");
+            n1 = scanner.nextInt();
+            System.out.println("Enter your Second  number");
+            n2 = scanner.nextInt();
+            n3 = n1 % n2;
+            System.out.println(n3);
+            break;
+        case 5:
+            System.out.println("Enter your first number");
+            n1 = scanner.nextInt();
+            System.out.println("Enter your Second  number");
+            n2 = scanner.nextInt();
+            n3 = n1 / n2;
+            System.out.println(n3);
+            break;
+        default:
+            System.err.println("Wrong Input");
+            break;
+    }
+    System.out.println("Do you want to exit? Press y for Yes and any other key for No");
+    ch = scanner.next().charAt(0);
+    }while(ch != 'y');
         }
     }
 
